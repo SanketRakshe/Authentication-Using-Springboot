6adf65b6dbbd9884ba4dff8c24d60e66326de3a8
SanketRakshe <sanketrakshe07@gmail.com>
2024-09-25 14:43:51 +0530

    Finall

--- src/main/java/com/example/jwtauth/security/SecurityConfig.java
+++ src/main/java/com/example/jwtauth/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.jwtauth.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
