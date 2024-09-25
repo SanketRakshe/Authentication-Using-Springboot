ad7e2b2d9d7a79936534e5fa05c7bbbf8470bbb0
SanketRakshe <sanketrakshe07@gmail.com>
2024-09-25 14:27:11 +0530

    Added a Auth controller to register user

---
+++ src/main/java/com/example/jwtauth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.jwtauth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jwtauth.entity.User;
+import com.example.jwtauth.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        userService.save(user);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
